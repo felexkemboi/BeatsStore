bfe22d8b066e6c29f50b3819e0cb72ccb05df8e9
felexkemboi <felokemboi10@gmail.com>
2018-05-08 17:20:23 +0300

    remove files to be ignored during pushing to master

--- musicstore/beats/urls.py
+++ musicstore/beats/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',                    views.shop,             name='shop'),
+    path('',                    views.shop,             name ='shop'),
     path('producers/',          views.allproducers,     name ='allproducers'),
     path('allbeats/',           views.allbeats,         name ='allbeats'),
     path('producers/<int:pk>/', views.producerbeats,    name ='producerbeats'),
